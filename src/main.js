ee9b2f65e3e63c571bf6efc47685a45132f1cefa
하승아 <hsa20130614@gmail.com>
2019-05-13 22:51:04 +0900

    vue routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = {template: '<div>Login</div>'}
+
+const routes = {
+  '/': App,
+  '/login': Login
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] ||
+        {template: '<div>Not Found</div>'}
+    }
+  },
+  render(h){
+    return h(this.VueComponent)
+  }
 })
